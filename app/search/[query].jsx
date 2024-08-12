7add93ec4fd05a32b1f82781b2391245d0461656
Sahel <abrahamjsahel@gmail.com>
2024-08-12 12:24:22 -0400

    Added firestore writing and loading screen mechanics

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,19 +1,48 @@
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Image, ActivityIndicator } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Image, ActivityIndicator, Alert } from 'react-native'
 import { React, useState, useEffect } from 'react'
 
 import { useLocalSearchParams, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { icons } from '../../constants'
-import { DATABASE } from '../(auth)/firebase';
+import { DATABASE, FIREBASE_AUTH } from '../(auth)/firebase';
+import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc, query, where, setDoc } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth';
 
 
 const Search = () => {
   const {query} = useLocalSearchParams();
+  const [isLoading, setIsLoading] = useState(true);
   const [response, setResponse] = useState('');
+  const [press, setPress] = useState(true)
+  const [saved, setSaved] = useState('#cdcde0')
+  const auth = getAuth();
+  const [id, setId] = useState()
+  const user = auth.currentUser;
   const hardPrompt = 'You\'re an expert health care researcher who wants to obtain a summarized information about a disease and any progress that has been made. Don\'t make up any details or hallucinate information. Your response should be in two paragraphs or less'
 
-    const fetchData = async => {
-      fetch(`http://35.193.161.194:2000/services/pubmed_abstracts/`,{
+  const fetchBookmarks = async () => {
+
+  }
+  const addBookmark = async () => {
+    console.log('Started')
+    if(user){
+      console.log('Trying')
+      await setDoc(doc(DATABASE, "User_Bookmarked", query ),{query: {query}, response: {response}, userId: user.uid})
+      console.log('Success!')
+    }
+    else{
+      Alert.alert('Error', 'No User Logged In')
+      console.log('Failed!')
+    }
+  }
+  const deleteBookmark = (id) => {
+    console.log('Attempting Deletion')
+    const document = doc(DATABASE, 'User_Bookmarked', id)
+  }
+
+
+    const fetchData = async () => {
+       await fetch(`http://35.193.161.194:2000/services/pubmed_abstracts/`,{
         method: 'POST',
         headers: new Headers({
           'Content-Type': 'application/json'
@@ -28,7 +57,9 @@ const Search = () => {
       }).then(resp => resp.json())
       .then(data => { setResponse(JSON.stringify(data).replaceAll('\",\"success\":true', '')
       .replaceAll('\"results\":\"','') .replaceAll('\\n', '\n').slice(1,-1)) })
-      .catch(err => { console.log(err) }); };
+      .catch(err => { console.log(err) }); 
+      setIsLoading(false)
+    };
 
       useEffect(() => {
         fetchData();
@@ -36,19 +67,37 @@ const Search = () => {
 
 
 
-      // if(isLoading){
-      //   return(
-      //     <SafeAreaView className="flex-1 bg-primary justify-center items-center">
-      //       <ActivityIndicator size='large' color='white'>
-      //       </ActivityIndicator>
-      //       <Text className="text-white mt-2">Loading...</Text>
-      //     </SafeAreaView>
-      //   )
-      // }
+      if(isLoading){
+        return(
+          <SafeAreaView className="flex-1 bg-primary justify-center items-center">
+            <ActivityIndicator size='large' color='white'>
+            </ActivityIndicator>
+            <Text className="text-white mt-2">Loading Results...</Text>
+          </SafeAreaView>
+        )
+      }
 
     const bookmarkQuery = async => {
-      
     };
+    
+    const pressed = () => {
+      setPress(!press)
+      if (press){
+        setSaved('#00CED1')
+        Alert.alert('Confirmation', 'Save this search to bookmarks?', [
+          { text: 'Cancel', onPress: () => {setSaved('#cdcde0'), setPress(true)}, },
+          { text: 'OK', onPress: () => {console.log('OK Pressed'), addBookmark()} },
+        ],)
+      }
+      else{
+        setSaved('#cdcde0')
+        Alert.alert('Confirmation', 'Delete this bookmarked search?', [
+          { text: 'Cancel', onPress: () => {setSaved('#00CED1'), setPress(false)}, },
+          { text: 'OK', onPress: () => {console.log('Ok2 Pressed')} },
+        ],)
+      }
+
+    }
 
   return (
       <SafeAreaView className="bg-primary h-full">
@@ -61,9 +110,24 @@ const Search = () => {
                   resizeMode='contain'
               />
               <Text className="text-white">Back to Search</Text>
-          </TouchableOpacity>
+            </TouchableOpacity>
             <View style={styles.container}>
-              <Text className= "text-xl text-black font-pbold">{query}:</Text>
+              <View className="flex-row flex-wrap">
+                <Text className= "text-xl text-black font-pbold mr-5" >{query}     
+                <TouchableOpacity
+                  onPress={pressed}
+                >
+                  <Image
+                    source={icons.bookmark}
+                    className="w-5 h-5 ml-2"
+                    resizeMode='contain'
+                    tintColor={saved}
+                  />
+                </TouchableOpacity>
+
+                </Text>
+                
+              </View>
               <Text></Text>
               <Text className= "text-l text-white font-psemibold"> {response} </Text>
           </View>
